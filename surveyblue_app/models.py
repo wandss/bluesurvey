87144128aefab6e3788b879f830f277b6cdf9dda
Anderson Luiz Neto <anderson.neto26@gmail.com>
2019-07-16 23:42:52 -0300

    Implementations relationships

--- surveyblue_app/models.py
+++ surveyblue_app/models.py
@@ -23,6 +23,8 @@ class Question(models.Model):
 
     description = models.CharField(max_length=500)
     question_type = models.IntegerField(choices=QUESTION_TYPE)
+    options = models.ManyToManyField(OptionResponse,
+                                     through='Question_OptionResponse')
 
     def __str__(self):
         return self.description
@@ -44,6 +46,9 @@ class Survey(models.Model):
     published_date = models.DateTimeField(blank=True, null=True)
     author = models.CharField(max_length=100)
     status = models.IntegerField(choices=STATUS)
+    questions = models.ManyToManyField(Question,
+                                     through='Survey_Question')
+
 
     def publish(self):
         self.published_date = timezone.now()
@@ -61,6 +66,27 @@ class Client(models.Model):
     email = models.EmailField(unique=True)
     cpf = models.CharField(max_length=11, unique=True)
     create_date = models.DateTimeField(default=timezone.now)
+    surveys = models.ManyToManyField(Survey,
+                                     through='Client_Survey')
+
 
     def __str__(self):
         return self.name
+
+
+class Question_OptionResponse(models.Model):
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    option_response = models.ForeignKey(OptionResponse,
+                                        on_delete=models.CASCADE)
+
+
+class Survey_Question(models.Model):
+
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+class Client_Survey(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
